21fa1fb3a27bb9be0665eca7b22871bcc0e96c79
harsha Wilson <nadarharshaw@gmail.com>
2025-12-12 13:50:53 +0530

    Remove unused import and establish Snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -10,8 +9,8 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write('The name on the smoothie will be :',name_on_order)
 
-
-
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
